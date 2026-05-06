d7f24f963fb176f7b220662a8be2f82380138b9c
ciandre <a.stanisic.codealkonost@gmail.com>
2026-05-06 19:55:18 +0200

    simple shhere

--- src/main.js
+++ src/main.js
@@ -1,4 +1,30 @@
 import "./style.css";
 import * as THREE from "three";
 
-console.log("Hello, Three.js!", THREE);
+const canvas = document.querySelector("#canvas");
+
+const scene = new THREE.Scene();
+const geometry = new THREE.SphereGeometry(1, 32, 32);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere);
+
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  1000,
+);
+camera.position.z = 5;
+scene.add(camera);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
