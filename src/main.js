6639dcf1a453d56d9683dea829dcf390edc00528
Aleksandar Stanisic <ciandre23@gmail.com>
2026-05-11 17:46:01 +0200

    default scene with Orbit controls

--- src/main.js
+++ src/main.js
@@ -1,6 +1,31 @@
 import "./style.css";
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 const canvas = document.querySelector("#canvas");
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000,
+);
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(window.innerWidth, window.innerHeight);
 
-console.log("Hello Three.js!", canvas, THREE);
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+camera.position.z = 5;
+function animate() {
+  requestAnimationFrame(animate);
+  controls.update();
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  renderer.render(scene, camera);
+}
+animate();
