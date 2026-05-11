387b69a69df228f6ca52b6550384c5cbd8f4e119
ciandre <a.stanisic.codealkonost@gmail.com>
2026-05-11 15:13:56 +0200

    Initialize Three.js scene with box geometry and camera setup

--- src/main.js
+++ src/main.js
@@ -1,2 +1,33 @@
 import "./style.css";
 import * as THREE from "three";
+const canvas = document.querySelector("#canvas");
+
+console.log("Hello Three.js!", canvas, THREE);
+
+const scene = new THREE.Scene();
+
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const boxMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const box = new THREE.Mesh(boxGeometry, boxMaterial);
+scene.add(box);
+box.position.x = -1.1;
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100,
+);
+camera.position.z = 3;
+scene.add(camera);
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  antialias: true,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.render(scene, camera);
