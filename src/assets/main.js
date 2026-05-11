323b32d06d698ce8a33e6fd45fffe9f10f3221fb
Aleksandar Stanisic <ciandre23@gmail.com>
2026-05-11 17:35:38 +0200

    Add main.js file with initial Three.js setup and rendering

---
+++ src/assets/main.js
@@ -0,0 +1,7 @@
+import "./style.css";
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+const canvas = document.querySelector("#canvas");
+
+console.log("Hello Three.js!", canvas, THREE);
