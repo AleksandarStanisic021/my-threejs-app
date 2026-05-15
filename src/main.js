b10147acf2a151d0aa61a9acffbf7bbb4ce073bb
Aleksandar Stanisic <ciandre23@gmail.com>
2026-05-15 15:15:56 +0200

    initialize scene, camera, and renderer with basic cube geometry

--- src/main.js
+++ src/main.js
@@ -1,4 +1,35 @@
 import "./style.css";
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 console.log(THREE);
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x444444);
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000,
+);
+camera.position.z = 3;
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshNormalMaterial({});
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube, camera);
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.update();
+
+console.log(`yoyo`);
+function animate() {
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+}
+
+animate();
