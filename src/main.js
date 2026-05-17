07ac3e142510de3294b2d653f3af08885bde9880
Aleksandar Stanisic <ciandre23@gmail.com>
2026-05-17 14:16:05 +0200

    initial setup

--- src/main.js
+++ src/main.js
@@ -1,34 +1,3 @@
-import "./style.css";
 import * as THREE from "three";
+import "./style.css";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-
-const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x444444);
-
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000,
-);
-camera.position.z = 3;
-
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshNormalMaterial({});
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube, camera);
-
-const renderer = new THREE.WebGLRenderer({ antialias: true });
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-controls.update();
-
-function animate() {
-  renderer.render(scene, camera);
-  requestAnimationFrame(animate);
-}
-
-animate();
